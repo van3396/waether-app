27e3182613929e47929c3ab90197cf7b738072b4
Qui <quivan3396@yahoo.com>
2021-02-23 01:23:39 -0500

    feat: add error handling to weather stack

--- app.js
+++ app.js
@@ -3,18 +3,25 @@ require("dotenv").config();
 
 const url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_API}&query=37.8267,-122.4233&units=f`;
 
-// request({ url: url, json: true }, (err, res) => {
-//   const { weather_descriptions,temperature, feelslike } = res.body.current;
-//   console.log(
-//     `${weather_descriptions[0]}. It is currently ${temperature} degrees out.  It feels like ${feelslike} degrees out.`
-//   );
-// });
+request({ url: url, json: true }, (err, res) => {
+  if (err) {
+    console.log("Unable to connect to weather service!");
+  } else if (res.body.error) {
+    console.log("Unable to find location.");
+  } else {
+    const { weather_descriptions, temperature, feelslike } = res.body.current;
 
-const mapboxURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${process.env.MAPBOX_API}&limit=1`;
-
-request({ url: mapboxURL, json: true }, (err, res) => {
-  const {features} = res.body
-  const lat = features[0].center[0]
-  const long = features[0].center[1]
-  console.log(`lat: ${lat} long: ${long}`)
+    console.log(
+      `${weather_descriptions[0]}. It is currently ${temperature} degrees out.  It feels like ${feelslike} degrees out.`
+    );
+  }
 });
+
+// const mapboxURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${process.env.MAPBOX_API}&limit=1`;
+
+// request({ url: mapboxURL, json: true }, (err, res) => {
+//   const { features } = res.body;
+//   const lat = features[0].center[1];
+//   const long = features[0].center[0];
+//   console.log(`lat: ${lat} long: ${long}`);
+// });
